9776253f1814dcb2693660b012fe39e4cc5fc5b2
Vlad Shcherban <vlad@coronalabs.com>
2020-06-06 20:11:26 +0300

    Updating binaries

---
+++ plugins/2015.2731/android/plugin_storeView.lua
@@ -0,0 +1,25 @@
+local Library = require "CoronaLibrary"
+
+-- Create library
+local lib = Library:new{name = 'plugin.storeView', publisherId = 'tech.scotth', version = 1.0}
+
+-------------------------------------------------------------------------------
+-- BEGIN (Insert your implementation startine here)
+-------------------------------------------------------------------------------
+
+-- This sample implements the following Lua:
+--
+--    local PLUGIN_NAME = require "plugin_PLUGIN_NAME"
+--    PLUGIN_NAME:showPopup()
+--
+local function showWarning()
+	native.showAlert('Not Supported', 'The StoreView plugin is only supported on an iOS device. Please build for device', {'OK'})
+end
+
+lib.init = showWarning
+lib.load = showWarning
+lib.isLoaded = showWarning
+lib.show = showWarning
+
+-- Return an instance
+return lib
